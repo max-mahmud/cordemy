4cbf679a7ee0027b4116572a180e47be40628634
max-mahmud <newmahmud24@gmail.com>
2023-07-02 00:35:52 +0600

    auth done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,38 +1,52 @@
-import Link from 'next/link'
-import Button from './Button';
+import Link from "next/link";
+import Button from "./Button";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
-    return (
-        <div className="h-20 bg-black text-gray-400 flex items-center">
-            <div className="wrapper flex justify-between items-center overflow-hidden">
-                <Link href="/" className="text-white font-semibold">
-                    Cordemy
-                </Link>
+  const { data: session } = useSession();
+  return (
+    <div className="h-20 bg-black text-gray-400 flex items-center">
+      <div className="wrapper flex justify-between items-center overflow-hidden">
+        <Link href="/" className="text-white font-semibold">
+          Cordemy
+        </Link>
 
-                <div className="flex gap-5">
-                    <Link href="/" className="hover:text-white transition-color">
-                        Home
-                    </Link>
-                    <Link href="/courses" className="hover:text-white transition-color">
-                        Courses
-                    </Link>
-                    <Link href="/orders" className="hover:text-white transition-color">
-                        Orders
-                    </Link>
+        <div className="flex gap-5">
+          <Link href="/" className="hover:text-white transition-color">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-color">
+            Courses
+          </Link>
+          <Link href="/orders" className="hover:text-white transition-color">
+            Orders
+          </Link>
 
-                    <Link href="/about" className="hover:text-white transition-color">
-                        About
-                    </Link>
-                    <Link href="/contact" className="hover:text-white transition-color">
-                        Contact
-                    </Link>
-                </div>
-                <div>
-                    <Button href="/users/login" placeholder="Sign in" color="secondary" />
-                </div>
-            </div>
+          <Link href="/about" className="hover:text-white transition-color">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-color">
+            Contact
+          </Link>
         </div>
-    )
-}
+        <div>
+          {!session ? (
+            <Button
+              href="/users/login"
+              placeholder="Sign in"
+              color="secondary"
+            />
+          ) : (
+            <Button
+              href="/users/profile"
+              placeholder="Profile"
+              color="secondary"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.pexels.com"],
-  }
-}
+    domains: ["images.pexels.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
